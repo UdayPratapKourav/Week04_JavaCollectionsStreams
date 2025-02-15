a60c108da99f3db998ef1c167f9bee36ce911f86
UdayPratapKourav <udaypratpkourav19@gmail.com>
2025-02-15 10:02:19 +0530

    [Uday] Add. finally block execution program

---
+++ Day04_Exceptions/src/main/java/com/finallyblockexecution/Main.java
@@ -0,0 +1,23 @@
+package com.finallyblockexecution;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        try{
+            System.out.println("Enter the first number :");
+            int a = sc.nextInt();
+            System.out.println("Enter the second number :");
+            int b = sc.nextInt();
+            System.out.println("Output" +a/b);
+        }catch (ArithmeticException e){
+            System.out.println("Enter only positive number ");
+        }finally {
+            System.out.println("Operation completed");
+        }
+    }
+}
