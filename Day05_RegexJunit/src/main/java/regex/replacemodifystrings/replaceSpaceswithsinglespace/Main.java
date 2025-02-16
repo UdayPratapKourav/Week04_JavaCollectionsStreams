edc7c57707e3428f67bafdaf0ca5e6ee9c4fd263
UdayPratapKourav <udaypratpkourav19@gmail.com>
2025-02-16 23:20:06 +0530

    [Uday] Add. replace space with single space

---
+++ Day05_RegexJunit/src/main/java/regex/replacemodifystrings/replaceSpaceswithsinglespace/Main.java
@@ -0,0 +1,10 @@
+package regex.replacemodifystrings.replaceSpaceswithsinglespace;
+
+public class Main {
+    public static void main(String[] args) {
+        String text="This is    an example with multiple spaces.";
+       String result= text.replaceAll("\\s+"," ");
+        System.out.println("Original : "+text);
+        System.out.println("Modified : "+result);
+    }
+}
