e80846c935643deeac9031a61c4ff05eaad5014e
UdayPratapKourav <udaypratpkourav19@gmail.com>
2025-02-17 15:08:36 +0530

    [Uday] Add. invert map

---
+++ Day02_Collections/src/main/java/com/mapinterface/invertmap/MapInverter.java
@@ -0,0 +1,32 @@
+package com.mapinterface.invertmap;
+
+
+
+import java.util.*;
+
+public class MapInverter {
+
+    public static <K, V> Map<V, List<K>> invertMap(Map<K, V> inputMap) {
+        Map<V, List<K>> invertedMap = new HashMap<>();
+
+        for (Map.Entry<K, V> entry : inputMap.entrySet()) {
+            V value = entry.getValue();
+            K key = entry.getKey();
+
+            invertedMap.computeIfAbsent(value, k -> new ArrayList<>()).add(key);
+        }
+
+        return invertedMap;
+    }
+
+    public static void main(String[] args) {
+        Map<String, Integer> inputMap = new HashMap<>();
+        inputMap.put("A", 1);
+        inputMap.put("B", 2);
+        inputMap.put("C", 1);
+
+        Map<Integer, List<String>> invertedMap = invertMap(inputMap);
+
+        System.out.println("Inverted Map: " + invertedMap);
+    }
+}
