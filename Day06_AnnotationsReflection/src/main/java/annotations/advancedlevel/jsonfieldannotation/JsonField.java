f7bd5f8ae7f3e3135abd882f00254697727d6123
UdayPratapKourav <udaypratpkourav19@gmail.com>
2025-02-23 22:58:20 +0530

    [Uday] Add. json field annotation

---
+++ Day06_AnnotationsReflection/src/main/java/annotations/advancedlevel/jsonfieldannotation/JsonField.java
@@ -0,0 +1,10 @@
+package annotations.advancedlevel.jsonfieldannotation;
+
+import java.lang.annotation.*;
+
+@Retention(RetentionPolicy.RUNTIME) // Annotation available at runtime
+@Target(ElementType.FIELD) // Can be used on fields only
+public @interface JsonField {
+    String name(); // Custom JSON key name
+}
+
